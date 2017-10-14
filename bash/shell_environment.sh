55e1118a3e90a54d2ef870a8a397b03a7f5cdf6e
Michael Schaarschmidt <michael.schaarschmidt@itz.uni-halle.de>
2017-10-15 00:07:05 +0200

    fix typo

--- bash/shell_environment.sh
+++ bash/shell_environment.sh
@@ -12,10 +12,7 @@ export PATH=${HOME}/.rbenv/bin:${PATH}
 # everything about my package manager nixos
 if [ "Darwin" == ${os} ]
 then
-  echo "hello Darwin"
   export PATH=/nix/var/nix/profiles/per-user/nmaef/profile/bin:${PATH}
-else
-  echo "Hello Linux"
 fi
 
 # my personal stuff is most important :D
